a7a8763ecbc507489f13506c799a7994cb81ee95
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-06-02 17:22:18 +0300

    Challenge 1

--- Prototyp1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Prototyp1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(17.2f, 0.8f, 8.7f);
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +14,7 @@ public class FollowPlayerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = plane.transform.position + offset;
     }
